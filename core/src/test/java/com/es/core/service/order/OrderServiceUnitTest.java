ce3debb9ebd9fc8b5999bde1a700df85b2e33e00
NikZa <nikitos86902002@gmail.com>
2021-11-29 17:28:46 +0300

    Task 2.8: Added tests

---
+++ core/src/test/java/com/es/core/service/order/OrderServiceUnitTest.java
@@ -0,0 +1,103 @@
+package com.es.core.service.order;
+
+import com.es.core.exception.OutOfStockException;
+import com.es.core.model.cart.Cart;
+import com.es.core.model.cart.CartItem;
+import com.es.core.model.order.Order;
+import com.es.core.model.order.OrderItem;
+import com.es.core.model.phone.Phone;
+import com.es.core.model.stock.Stock;
+import com.es.core.service.stock.StockService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class OrderServiceUnitTest {
+    @Mock
+    private StockService stockService;
+    @Mock
+    private Cart cart;
+    @Mock
+    private CartItem cartItem1;
+    @Mock
+    private CartItem cartItem2;
+    @Mock
+    private Order order;
+    @Mock
+    private Stock stock;
+    @Mock
+    private Phone phone;
+    @Spy
+    private OrderItem orderItem;
+    @Spy
+    private ArrayList<CartItem> cartItems;
+    @Spy
+    private ArrayList<OrderItem> orderItems;
+
+    private Integer deliveryPrice;
+    private OrderService orderService;
+
+    @Before
+    public void setup() {
+        deliveryPrice = 5;
+        orderService = new OrderServiceImpl(deliveryPrice, stockService);
+
+        cartItems.add(cartItem1);
+        cartItems.add(cartItem2);
+
+        orderItems.add(orderItem);
+
+        when(orderItem.getPhone()).thenReturn(phone);
+        when(orderItem.getPhone().getId()).thenReturn(100L);
+        when(orderItem.getQuantity()).thenReturn(10L);
+
+        when(phone.getId()).thenReturn(100L);
+
+        when(cart.getItems()).thenReturn(cartItems);
+        when(cart.getTotalCost()).thenReturn(BigDecimal.valueOf(500));
+
+        when(stockService.getStock(anyLong())).thenReturn(Optional.of(stock));
+    }
+
+    @Test
+    public void shouldCreateOrder() {
+        orderService.createOrder(cart);
+
+        verify(cart, times(2)).getTotalCost();
+        verify(cart).getItems();
+        verify(cartItem1).getQuantity();
+        verify(cartItem1).getPhone();
+        verify(cartItem2).getQuantity();
+        verify(cartItem2).getPhone();
+    }
+
+    @Test(expected = OutOfStockException.class)
+    public void shouldPlaceOrderWithOutOfStock() {
+        when(order.getOrderItems()).thenReturn(orderItems);
+
+        orderService.placeOrder(order);
+    }
+
+    @Test
+    public void shouldPlaceOrder() {
+        when(stock.getStock()).thenReturn(1000);
+        when(stockService.getStock(anyLong())).thenReturn(Optional.of(stock));
+
+        when(order.getOrderItems()).thenReturn(orderItems);
+
+        orderService.placeOrder(order);
+    }
+}
--- web/src/main/java/com/es/phoneshop/web/controller/pages/OrderOverviewPageController.java
+++ web/src/main/java/com/es/phoneshop/web/controller/pages/OrderOverviewPageController.java
@@ -1,9 +1,16 @@
 package com.es.phoneshop.web.controller.pages;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "/orderOverview")
 public class OrderOverviewPageController {
+
+    @GetMapping("/{secureId}")
+    public String getOrderOverview(@PathVariable("secureId") String secureId){
+        return "orderOverview";
+    }
 }
